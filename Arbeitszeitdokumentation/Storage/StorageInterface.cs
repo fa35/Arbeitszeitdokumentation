7b327129b2e878df5bf7ce848fe920b48ef77ca0
Michael Große <mic.grosse+github@googlemail.com>
2015-10-20 00:17:52 +0200

    Add storage interface

---
+++ Arbeitszeitdokumentation/Storage/StorageInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Arbeitszeitdokumentation.Container;
+
+namespace Arbeitszeitdokumentation.Storage
+{
+    interface IStorage
+    {
+        List<Employee> GetEmployees();
+        List<Project> GetProjects();
+        List<Worklog> GetWorklogs();
+
+        void AddEmployee(Employee employee);
+        void AddProject(Project project);
+        void AddWorklog(Worklog worklog);
+
+        void UpdateEmployee(Employee employee);
+        void UpdateProject(Project project);
+        void UpdateWorklog(Worklog worklog);
+
+        void DeleteEmployee(int id);
+        void DeleteProject(int id);
+        void DeleteWorklog(int id);
+
+    }
+}
