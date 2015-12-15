b833c7873456b6ad90a74d37656160947fab30d9
Michael Große <mic.grosse@posteo.de>
2015-12-15 13:59:02 +0100

    Add explaination for sort decorator

--- Arbeitszeitdokumentation/Program.cs
+++ Arbeitszeitdokumentation/Program.cs
@@ -9,8 +9,9 @@ namespace Arbeitszeitdokumentation
         static void Main(string[] args)
         {
 
-            var app = new TUI(new SortedBusinessLogic(new MockStorage()));
-
+            var app = new TUI(new SortedBusinessLogic(new Proxy(new MockStorage())));
+            // = new TUI(new SortedBusinessLogic(new Proxy(new MockStorage())));  //<-- sortiert
+            // = new TUI(new Proxy(new MockStorage()));  //<-- nicht sortiert
         }
     }
 }
